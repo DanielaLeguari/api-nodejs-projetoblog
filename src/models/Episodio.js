c78f17481814c654cb19c5b06a6d86f0b1aa13c3
Daniela Leguari <danielaleguari@outlook.com>
2023-02-22 13:41:07 -0300

    adicionado cors

--- src/models/Episodio.js
+++ src/models/Episodio.js
@@ -5,7 +5,9 @@ const episodioSchema = new mongoose.Schema(
         id: { type: String },
         titulo: { type: String, required: true },
         texto: { type: String, required: true},
-        favorito: {type: Boolean}
+        favorito: {type: Boolean},
+        temporada: {type: Number,required: true},
+        numero: {type: Number,required: true}
 
     },
     {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import episodios from "./episodiosRoutes.js";
+import cors from "cors";
 
 const routes = (app) => {
+    app.use(cors());
     app.route('/').get((req, res) => {
         res.status(200).send({ titulo : "projeto blog"})
     });
